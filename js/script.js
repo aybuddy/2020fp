fe88b44bd5c2959d951f59230404094b0cc78c94
Aaron Yu <ayybuddyy@gmail.com>
2020-11-25 20:55:18 -0500

    Add sass styles, compile

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ new fullpage("#fullpage", {
   navigation: true,
   anchors: ["about", "projects", "contact", "more"],
 });
+
+gsap.from(".container", {
+  opacity: 0,
+  duration: 1,
+  y: -50,
+  ease: "Power2.easeInOut",
+});
+
+// gsap.from()
